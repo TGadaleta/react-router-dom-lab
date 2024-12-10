8b5a0db174270cf11b86f7883ca02963c275c0a6
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-12-10 13:20:44 -0500

    nav bar and routes written

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar';
+import MailboxDetails from './components/MailboxDetails';
+import MailboxForm from './components/MailboxForm';
+import MailboxList from './components/MailboxList';
 import './App.css'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <NavBar />
+      <h1>Test</h1>
+      <Routes>
+      <Route path='/' element={<mail><h1>Post Office</h1></mail>} />
+      <Route path='/mailboxes' element={<MailboxList />} />
+      <Route path='/new-mailbox' element={<MailboxForm />} />
+      <Route path='/mailboxes/:mailboxId' element={<MailboxDetails />} />
+      </Routes>
+    </>
+  )
 };
 
 export default App
